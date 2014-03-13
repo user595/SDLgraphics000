7811e73d08a69d9208a47a6978714c1ceeda62d8
user595 <dwightwilkins@gmail.com>
2014-03-13 13:29:22 -0500

    display bmp image file

--- SDLgraphics000/SDLgraphics000/main.cpp
+++ SDLgraphics000/SDLgraphics000/main.cpp
@@ -1,9 +1,26 @@
 #include "SDL.h"
 int main( int argc, char* args[] )
 {
-//Start SDL
-SDL_Init( SDL_INIT_EVERYTHING );
-//Quit SDL
-SDL_Quit();
-return 0;
+SDL_Surface* hello = NULL;
+    SDL_Surface* screen = NULL;SDL_Init( SDL_INIT_EVERYTHING );
+
+    //Set up screen
+    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+
+    //Load image
+    hello = SDL_LoadBMP( "hello_world.bmp" ); //Apply image to screen
+    SDL_BlitSurface( hello, NULL, screen, NULL );
+
+    //Update Screen
+    SDL_Flip( screen );
+
+    //Pause
+    SDL_Delay( 10000 );  /*2000 milliseconds is two seconds*/
+	//Free the loaded image
+    SDL_FreeSurface( hello );
+
+    //Quit SDL
+    SDL_Quit();
+
+    return 0;
 }
