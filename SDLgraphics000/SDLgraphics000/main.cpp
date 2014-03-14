4bd595205b55ff6974048b95f03b95d1e50334f8
user595 <dwightwilkins@gmail.com>
2014-03-13 20:24:08 -0500

    change individual pixels

--- SDLgraphics000/SDLgraphics000/main.cpp
+++ SDLgraphics000/SDLgraphics000/main.cpp
@@ -12,11 +12,20 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
 
+//Flip flags
+const int FLIP_VERTICAL = 1;
+const int FLIP_HORIZONTAL = 2;
+
 //The surfaces
 SDL_Surface *image = NULL;
 SDL_Surface *screen = NULL;
 SDL_Surface *dots = NULL;
 SDL_Surface *message = NULL;
+SDL_Surface *topLeft = NULL;
+SDL_Surface *topRight = NULL;
+SDL_Surface *bottomLeft = NULL;
+SDL_Surface *bottomRight = NULL;
+SDL_Surface *global_surface = NULL;
 
 //The event structure that will be used
 SDL_Event event;
@@ -123,6 +132,146 @@ bool init()
     return true;
 }
 
+Uint32 get_pixel32( SDL_Surface *surface, int x, int y )
+{
+    //Convert the pixels to 32 bit
+    Uint32 *pixels = (Uint32 *)surface->pixels;
+
+    //Get the requested pixel
+    return pixels[ ( y * surface->w ) + x ];
+}
+
+void put_pixel32( SDL_Surface *surface, int x, int y, Uint32 pixel )
+{
+    //Convert the pixels to 32 bit
+    Uint32 *pixels = (Uint32 *)surface->pixels;
+
+    //Set the pixel
+    pixels[ ( y * surface->w ) + x ] = pixel;
+}
+
+SDL_Surface *flip_surface( SDL_Surface *surface, int flags )
+{
+    //Pointer to the soon to be flipped surface
+    SDL_Surface *flipped = NULL;
+
+	flipped = surface;
+	return flipped;
+
+
+    //If the image is color keyed
+    if( surface->flags & SDL_SRCCOLORKEY )
+    {
+        flipped = SDL_CreateRGBSurface( SDL_SWSURFACE, surface->w, surface->h, surface->format->BitsPerPixel, surface->format->Rmask, surface->format->Gmask, surface->format->Bmask, 0 );
+    }
+    //Otherwise
+    else
+    {
+        flipped = SDL_CreateRGBSurface( SDL_SWSURFACE, surface->w, surface->h, surface->format->BitsPerPixel, surface->format->Rmask, surface->format->Gmask, surface->format->Bmask, surface->format->Amask );
+    }
+
+    //If the surface must be locked
+    if( SDL_MUSTLOCK( surface ) )
+    {
+        //Lock the surface
+        SDL_LockSurface( surface );
+    }
+
+    //Go through columns
+    for( int x = 0, rx = flipped->w - 1; x < flipped->w; x++, rx-- )
+    {
+        //Go through rows
+        for( int y = 0, ry = flipped->h - 1; y < flipped->h; y++, ry-- )
+        {
+            //Get pixel
+            Uint32 pixel = get_pixel32( surface, x, y );
+
+            //Copy pixel
+            if( ( flags & FLIP_VERTICAL ) && ( flags & FLIP_HORIZONTAL ) )
+            {
+                put_pixel32( flipped, rx, ry, pixel );
+            }
+            else if( flags & FLIP_HORIZONTAL )
+            {
+                put_pixel32( flipped, rx, y, pixel );
+            }
+            else if( flags & FLIP_VERTICAL )
+            {
+                put_pixel32( flipped, x, ry, pixel );
+            }
+        }
+    }
+
+    //Unlock surface
+    if( SDL_MUSTLOCK( surface ) )
+    {
+        SDL_UnlockSurface( surface );
+    }
+
+    //Copy color key 
+    if( surface->flags & SDL_SRCCOLORKEY )
+    {
+        SDL_SetColorKey( flipped, SDL_RLEACCEL | SDL_SRCCOLORKEY, surface->format->colorkey );
+    }
+
+    //Return flipped surface
+    return flipped;
+}
+
+SDL_Surface *change_surface( SDL_Surface *surface )
+{
+    //Pointer to the soon to be flipped surface
+    SDL_Surface *flipped = surface;
+
+    //If the image is color keyed
+    if( surface->flags & SDL_SRCCOLORKEY )
+    {
+        flipped = SDL_CreateRGBSurface( SDL_SWSURFACE, surface->w, surface->h, surface->format->BitsPerPixel, surface->format->Rmask, surface->format->Gmask, surface->format->Bmask, 0 );
+    }
+    //Otherwise
+    else
+    {
+        //flipped = SDL_CreateRGBSurface( SDL_SWSURFACE, surface->w, surface->h, surface->format->BitsPerPixel, surface->format->Rmask, surface->format->Gmask, surface->format->Bmask, surface->format->Amask );
+    }
+
+    //If the surface must be locked
+    if( SDL_MUSTLOCK( surface ) )
+    {
+        //Lock the surface
+        SDL_LockSurface( surface );
+    }
+
+    //Go through columns
+    for( int x = 0, rx = flipped->w - 1; x < flipped->w; x++, rx-- )
+    {
+		//break;
+        //Go through rows
+        for( int y = 0, ry = flipped->h - 1; y < flipped->h; y++, ry-- )
+        {
+            //Get pixel
+            Uint32 pixel = get_pixel32( surface, x, y );
+
+			put_pixel32( flipped, rx, ry, pixel );
+            
+        }
+    }
+
+    //Unlock surface
+    if( SDL_MUSTLOCK( surface ) )
+    {
+        SDL_UnlockSurface( surface );
+    }
+
+    //Copy color key
+    if( surface->flags & SDL_SRCCOLORKEY )
+    {
+        SDL_SetColorKey( flipped, SDL_RLEACCEL | SDL_SRCCOLORKEY, surface->format->colorkey );
+    }
+
+    //Return flipped surface
+    return flipped;
+}
+
 void clean_up()
 {
     //Free the surface
@@ -133,6 +282,11 @@ void clean_up()
 
     //Quit SDL_ttf
     TTF_Quit();
+
+    SDL_FreeSurface( topLeft );
+    SDL_FreeSurface( topRight );
+    SDL_FreeSurface( bottomLeft );
+    SDL_FreeSurface( bottomRight );
 
     //Quit SDL
     SDL_Quit();
@@ -217,17 +371,30 @@ int main( int argc, char* args[] )
     message = TTF_RenderText_Solid( font, "The quick brown fox jumps over the lazy frog", textColor );
 	apply_surface( 0, 150, message, screen );
 
-    //If there was an error in rendering the text
+    //If there was an error in rendering the text 
     if( message == NULL )
     {
         return 1;
     }
+
+    //Flip surfaces
+	//topLeft = screen;
+    topRight = change_surface( screen );
+    //bottomLeft = flip_surface( topLeft, FLIP_VERTICAL );
+    //bottomRight = flip_surface( screen, FLIP_HORIZONTAL | FLIP_VERTICAL );
+
+	//topLeft = flip_surface( screen, FLIP_HORIZONTAL | FLIP_VERTICAL );
+	//if (topLeft != screen)
+		//screen = topLeft;
+	
+	screen = topRight;
 
     //Update the screen
     if( SDL_Flip( screen ) == -1 )
     {
         return 1;
     }
+	
 
     //While the user hasn't quit
     while( quit == false )
@@ -241,6 +408,23 @@ int main( int argc, char* args[] )
                 //Quit the program
                 quit = true;
             }
+			if( event.type == SDL_KEYDOWN )
+			{
+			/*
+				screen = bottomRight;
+				SDL_Flip( screen );
+				break;
+				*/
+				switch( event.key.keysym.sym )
+                {
+                    case SDLK_UP: screen = topLeft; break;
+                    case SDLK_DOWN: screen = bottomLeft; break;
+                    case SDLK_LEFT: screen = topRight; break;
+                    case SDLK_RIGHT: screen = bottomRight; break;
+                }
+				//SDL_Flip( screen );
+
+			}
         }
     }
 
